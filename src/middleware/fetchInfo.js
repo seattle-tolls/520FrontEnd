4e9ccf6dc7184500a2002e9a6043383e30bb6a04
ronbarrantes <ronbarrantes@gmail.com>
2019-05-06 00:23:53 -0700

    Add initializers to the fetch request

--- src/middleware/fetchInfo.js
+++ src/middleware/fetchInfo.js
@@ -1,13 +1,23 @@
 import { FETCH_TOLL_INFO } from '../constants/action-types'
 import { setTollInfo } from '../actions/toll'
 const apiURL = process.env.API_URL
+const myHeaders = new Headers({
+
+})
+
+const inits = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
 
 const fetchInfo = ({ dispatch, getState }) => next => action => {
 
   if(action.type !== FETCH_TOLL_INFO)
     return next(action)
 
-  fetch(`${apiURL}/api/toll`)
+  fetch(`${apiURL}/api/toll`, inits)
     .then(res => {
       return res.json()
     })
